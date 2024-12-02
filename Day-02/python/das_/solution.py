e65a68ea70ab9ed4b4239ec5570457dd645adf2e
Daniel Scherf <35780068+dlsf@users.noreply.github.com>
2024-12-02 09:34:52 +0100

    Update day 02 Python solution again

--- Day-02/python/das_/solution.py
+++ Day-02/python/das_/solution.py
@@ -2,16 +2,11 @@ import itertools as it
 import math
 
 def safe(l):
-    t = [a - b for (a, b) in it.pairwise(l)]
-    return all(t[k] in range((j := int(math.copysign(1, t[0]))), 4*j, j) for k in range(len(t)))
+    diffs = [a - b for (a, b) in it.pairwise(l)]
+    return all(diffs[k] in range((j := int(math.copysign(1, diffs[0]))), 4*j, j) for k in range(len(diffs)))
 
 reports = [list(map(int, line.split())) for line in open("input.txt").readlines()]
-counter = [0, 0]
-for report in reports:
-    if safe(report):
-        counter = [e + 1 for e in counter]
-    else:
-        counter[1] += 1 if any(safe(report[:i] + report[(i + 1):]) for i in range(len(report))) else 0
+scores = [sum(safe(report) for report in reports), sum(any(safe(report[:i] + report[(i + 1):]) for i in range(len(report))) for report in reports)]
 
-print("Part 1:", counter[0])
-print("Part 2:", counter[1])
+print("Part 1:", scores[0])
+print("Part 2:", scores[1])
