597500cdff75d99041d7a3532343e60100d8066f
Paul Hoger <paul.hoger@gmx.net>
2024-12-03 18:16:35 +0100

    Solve part 2 and cleanup

--- Day-03/python/paul2708/day03.py
+++ Day-03/python/paul2708/day03.py
@@ -1,40 +1,40 @@
-from typing import List
-
 from shared.paul2708.input_reader import *
 from shared.paul2708.output import write
 
 lines = read_plain_input(day=3)
-
-total = 0
-for instruction in lines:
-
-    x = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", instruction)
-
-    for a in x:
-        total += int(a.replace("mul", "").replace("(", "").replace(")", "").split(",")[0]) * int(a.replace("mul", "").replace("(", "").replace(")", "").split(",")[1])
-
-print(total)
-
-lines = read_plain_input(day=3, example=1)
-
-total = 0
-for instruction in lines:
-    print(instruction)
-    instruction = re.sub(r"don't\(\)(.*?)do\(\)", r"", instruction)
-    print(instruction)
-    instruction = re.sub(r"don't\(\)?(.*)", r"", instruction)
-    print(instruction)
-
-    x = re.findall("mul\([0-9]{1,3},[0-9]{1,3}\)", instruction)
-    for a in x:
-        total += int(a.replace("mul", "").replace("(", "").replace(")", "").split(",")[0]) * int(
-            a.replace("mul", "").replace("(", "").replace(")", "").split(",")[1])
+instruction = "".join(lines)
 
 
-print(total)
+def process(content: str) -> int:
+    groups = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", content)
+
+    total = 0
+    for match in groups:
+        numbers = match.replace("mul", "").replace("(", "").replace(")", "").split(",")
+        total += int(numbers[0]) * int(numbers[1])
+
+    return total
 
 
-def validate(line: str):
-    i = 0
-    # Alles entfernen zwischen dont() und do() und dont()... (wenn kein do() mehr kommt)
-    while i < len(line):
+# Part 1
+write(f"Processing the mult operations results in a score of <{process(instruction)}>.")
+
+
+# Part 2
+def remove_skipped_operations(line: str) -> str:
+    if "don't()" not in line:
+        return line
+
+    dont_index = line.index("don't()")
+
+    if "do()" not in line[dont_index:]:
+        return line[:dont_index]
+
+    do_index = line.index("do()", dont_index)
+
+    skip = line[:dont_index] + line[do_index:]
+    return remove_skipped_operations(skip)
+
+
+skipped_operations = remove_skipped_operations(instruction)
+write(f"Skipping disabled operations results in a score of <{process(skipped_operations)}>.")
