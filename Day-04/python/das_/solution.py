e105ab3a5c223b6d64cb73330e456794da860d35
Daniel Scherf <35780068+dlsf@users.noreply.github.com>
2024-12-04 10:30:49 +0100

    Update day 04 Python solution

--- Day-04/python/das_/solution.py
+++ Day-04/python/das_/solution.py
@@ -8,9 +8,9 @@ def part1(row, column, direction, length=0):
              part1(row + direction[0], column + direction[1], direction, length + 1)))
 
 def part2(row, column):
-    word1 = grid[row - 1][column - 1] + grid[row + 1][column + 1]
-    word2 = grid[row + 1][column - 1] + grid[row - 1][column + 1]
-    return 1 if word1 in {"MS", "SM"} and word2 in {"MS", "SM"} else 0
+    word1 = grid[row - 1][column - 1] + "A" + grid[row + 1][column + 1]
+    word2 = grid[row + 1][column - 1] + "A" + grid[row - 1][column + 1]
+    return True if word1 in {"MAS", "SAM"} and word2 in {"MAS", "SAM"} else False
 
-print(sum(part1(i, j, d) for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j] == "X" for d in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]))
-print(sum(part2(i, j) for i in range(1, len(grid) - 1) for j in range(1, len(grid[0]) - 1) if grid[i][j] == "A"))
+print("Part 1:", sum(part1(i, j, d) for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j] == "X" for d in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]))
+print("Part 2:", sum(part2(i, j) for i in range(1, len(grid) - 1) for j in range(1, len(grid[0]) - 1) if grid[i][j] == "A"))
