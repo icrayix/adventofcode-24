abf1f616d6639eeb33c15dffeb00ca43500d4177
Paul <Paul2708@users.noreply.github.com>
2024-12-05 16:16:24 +0100

    Solve part 2

--- Day-05/python/paul2708/day05.py
+++ Day-05/python/paul2708/day05.py
@@ -1,10 +1,12 @@
-from typing import List, Tuple
+from typing import List
+
+from functools import cmp_to_key
 
 from shared.paul2708.input_reader import *
 from shared.paul2708.output import write
 
-lines = read_plain_input(day=5, example=1)
-rules = [(int(line.split("|")[0]), int(line.split("|")[1])) for line in lines[:lines.index("")]]
+lines = read_plain_input(day=5)
+rules = [tuple(map(int, line.split("|"))) for line in lines[:lines.index("")]]
 updates = [list(map(int, line.split(","))) for line in lines[lines.index("") + 1:]]
 
 must_be_before = {}
@@ -14,38 +16,31 @@ for x, y in rules:
     must_be_before[x].append(y)
 
 
+# Part 1
 def is_correct(rule: List[int]) -> bool:
     for i in range(len(rule) - 1):
         current, following = rule[i], rule[i + 1]
 
-        if current not in must_be_before:
-            return False
         if current in must_be_before and not following in must_be_before[current]:
             return False
 
     return True
 
-print(must_be_before)
 
-total = 0
+# Part 2
+def compare(a: int, b: int) -> int:
+    return -1 if b in must_be_before[a] else 1
+
+
+# Run both parts
+part1 = 0
+part2 = 0
 for update in updates:
     if is_correct(update):
-        print(update)
-        total += update[len(update) // 2]
+        part1 += update[len(update) // 2]
+    else:
+        corrected_update = sorted(update, key=cmp_to_key(compare))
+        part2 += corrected_update[len(corrected_update) // 2]
 
-print(total)
-
-
-def sort(update: List[int]):
-    for n in range(len(update), 1, -1):
-        for i in range(n - 1):
-            if update[i] in must_be_before and update[i + 1] in must_be_before[update[i]]:
-                before = update[i]
-                after = update[i + 1]
-
-                update[i] = after
-                update[i + 1] = before
-
-x = [61, 13, 29]
-sort(x)
-print(x)
+write(f"The sum of all correct updated is <{part1}>.")
+write(f"The sum of all incorrect updated is <{part2}>.")
