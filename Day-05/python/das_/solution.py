cf8a1cb9cab800452cd22500975812d05753aa20
Daniel Scherf <35780068+dlsf@users.noreply.github.com>
2024-12-05 15:48:36 +0100

    Improve day 05 Python solution

--- Day-05/python/das_/solution.py
+++ Day-05/python/das_/solution.py
@@ -6,25 +6,24 @@ updates = [list(map(int, line.split(","))) for line in content[1].splitlines()]
 
 predecessors = dict()
 for rule in rules:
-    predecessors.setdefault(rule[1], [])
-    predecessors[rule[1]].append(rule[0])
+    predecessors.setdefault(rule[1], []).append(rule[0])
 
-s, t = 0, 0
+part1, part2 = 0, 0
 for update in updates:
     valid, swapped = False, False
     while not valid:
         valid = True
         for i in range(len(update) - 1):
-            for next_element in update[i + 1:]:
-                if next_element in predecessors.get(update[i], []):
-                    swapped, valid, ind = True, False, update.index(next_element)
+            for j in range(i + 1, len(update)):
+                if update[j] in predecessors.get(update[i], []):
+                    swapped, valid, ind = True, False, j
                     update[i], update[ind] = update[ind], update[i]
                     break
 
     if swapped:
-        t += update[int(len(update) / 2)]
+        part2 += update[int(len(update) / 2)]
     else:
-        s += update[int(len(update) / 2)]
+        part1 += update[int(len(update) / 2)]
 
-print("Part 1:", s)
-print("Part 2:", t)
+print("Part 1:", part1)
+print("Part 2:", part2)
