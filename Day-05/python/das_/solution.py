56c2a6cd4387f9a98fa5193438a51d1beb7636e5
Daniel Scherf <35780068+dlsf@users.noreply.github.com>
2024-12-05 17:54:48 +0100

    Update day 05 Python solution

--- Day-05/python/das_/solution.py
+++ Day-05/python/das_/solution.py
@@ -1,7 +1,5 @@
-import re
-
 content = open("input.txt").read().split("\n\n")
-rules = [list(map(int, re.findall(r"(\d+)\|(\d+)", line)[0])) for line in content[0].splitlines()]
+rules = [tuple(map(int, line.split("|"))) for line in content[0].splitlines()]
 updates = [list(map(int, line.split(","))) for line in content[1].splitlines()]
 
 predecessors = dict()
@@ -14,8 +12,8 @@ for update in updates:
     for i in range(len(update) - 1):
         for j in range(i + 1, len(update)):
             if update[j] in predecessors.get(update[i], []):
-                swapped, ind = True, j
-                update[i], update[ind] = update[ind], update[i]
+                swapped = True
+                update[i], update[j] = update[j], update[i]
 
     if swapped:
         part2 += update[int(len(update) / 2)]
