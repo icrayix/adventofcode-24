08fdd1b2be612531d807c0af49733e7179b4cd89
Daniel Scherf <munita.gavai@gmail.com>
2024-12-10 23:13:10 +0100

    Update day 10 Python solution

--- Day-10/python/das_/solution.py
+++ Day-10/python/das_/solution.py
@@ -10,9 +10,12 @@ def solve(row, column, n=0):
     if n == 9:
         return [(row, column)]
 
-    return solve(row + 1, column, n + 1) + solve(row, column + 1, n + 1) + solve(row - 1, column, n + 1) + solve(row, column - 1, n + 1)
+    return (solve(row + 1, column, n + 1) +
+            solve(row, column + 1, n + 1) +
+            solve(row - 1, column, n + 1) +
+            solve(row, column - 1, n + 1))
 
 grid = [list(map(int, line.strip())) for line in open("input.txt")]
 paths = [solve(*pos) for pos in itertools.product(range(len(grid)), range(len(grid[0])))]
-print("Part 1:", sum([len(set(path)) for path in paths]))
-print("Part 1:", sum([len(path) for path in paths]))
+print("Part 1:", sum(len(set(path)) for path in paths))
+print("Part 2:", sum(len(path) for path in paths))
