753e8c71eba388d546b676885622f6fb9bfe55da
Daniel Scherf <munita.gavai@gmail.com>
2024-12-13 20:35:34 +0100

    Update day 13 Python solution

--- Day-13/python/das_/solution.py
+++ Day-13/python/das_/solution.py
@@ -1,6 +1,6 @@
 import re
 
-def calculate_score(target):
+def calculate_tokens(target):
     tb = (target[1] * A[0] - target[0] * A[1]) / (B[1] * A[0] - B[0] * A[1])
     ta = (target[0] - B[0] * tb) / A[0]
     return int(ta * 3 + tb) if ta.is_integer() and tb.is_integer() else 0
@@ -10,8 +10,8 @@ part1, part2 = 0, 0
 
 for machine in machines:
     A, B, C = map(lambda line: list(map(int, re.findall(r"(\d+)\D+(\d+)", line)[0])), machine)
-    part1 += calculate_score(C)
-    part2 += calculate_score((C[0] + 10000000000000, C[1] + 10000000000000))
+    part1 += calculate_tokens(C)
+    part2 += calculate_tokens((C[0] + 10 ** 13, C[1] + 10 ** 13))
 
 print("Part 1:", part1)
 print("Part 2:", part2)
