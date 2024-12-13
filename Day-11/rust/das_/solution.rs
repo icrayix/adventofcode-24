ae5017446c779efdec231919ef2feaeb2491732b
Daniel Scherf <munita.gavai@gmail.com>
2024-12-13 22:04:18 +0100

    Update day 11 Rust solution

--- Day-11/rust/das_/solution.rs
+++ Day-11/rust/das_/solution.rs
@@ -2,32 +2,31 @@ use std::fs;
 
 // Just for fun, this approach obviously doesn't work for part 2
 fn main() {
-    let content = fs::read_to_string("input.txt").unwrap();
-    let mut numbers = Vec::new();
-    for x in content.split(" ") {
-        numbers.push(x.parse::<u128>().unwrap());
-    }
+    let mut stones: Vec<u128> = fs::read_to_string("input.txt").unwrap()
+        .split_whitespace()
+        .map(|stone| stone.parse::<u128>().unwrap())
+        .collect();
 
     for _ in 0..25 {
-        let mut new_numbers = Vec::new();
+        let mut new_stones = Vec::new();
 
-        for (_, stone) in numbers.iter().enumerate() {
+        for stone in stones {
             let str = stone.to_string();
 
-            if *stone == 0 {
-                new_numbers.push(1);
+            if stone == 0 {
+                new_stones.push(1);
             } else if str.len() % 2 == 0 {
                 let half = str.len() / 2;
-                new_numbers.push(str[0..half].parse::<u128>().unwrap());
-                new_numbers.push(str[half..].parse::<u128>().unwrap());
+                new_stones.push(str[0..half].parse::<u128>().unwrap());
+                new_stones.push(str[half..].parse::<u128>().unwrap());
             } else {
-                new_numbers.push(stone * 2024);
+                new_stones.push(stone * 2024);
             }
         }
 
-        numbers = new_numbers;
+        stones = new_stones;
     }
 
-    println!("Part 1: {}", numbers.len());
+    println!("Part 1: {}", stones.len());
     println!("Part 2: too slow")
 }
