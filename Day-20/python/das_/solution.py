8b0bf42ef14a2b6ce48e47530f1095205a236963
Daniel Scherf <munita.gavai@gmail.com>
2024-12-20 23:26:48 +0100

    Improve day 20 Python solution

--- Day-20/python/das_/solution.py
+++ Day-20/python/das_/solution.py
@@ -1,3 +1,9 @@
+from functools import cache
+
+@cache
+def is_in_bounds(x, y):
+    return 0 <= x < len(grid) and 0 <= y < len(grid[0])
+
 def find(char):
     return next((row, column) for row in range(len(grid)) for column in range(len(grid[0])) if grid[row][column] == char)
 
@@ -18,7 +24,7 @@ def bfs(pos, starting_weight=0):
 
         for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             new_x, new_y = location[0] + dx, location[1] + dy
-            if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]) and grid[new_x][new_y] != "#" and (new_x, new_y) not in weights:
+            if is_in_bounds(new_x, new_y) and grid[new_x][new_y] != "#" and (new_x, new_y) not in weights:
                 locations[(new_x, new_y)] = weight + 1
 
 grid = open("input.txt").read().splitlines()
@@ -32,7 +38,7 @@ part1 = 0
 for (x, y), weight in weights.items():
     for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
         new_x, new_y = x + 2 * dx, y + 2 * dy
-        if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]):
+        if is_in_bounds(new_x, new_y):
             if grid[x + dx][y + dy] == "#" and grid[new_x][new_y] != "#":
                 new_time = bfs((new_x, new_y), weights[(x, y)] + 2)
                 if new_time < initial_time:
@@ -44,20 +50,21 @@ print("Part 1:", part1)
 
 part2 = 0
 used_cheats = set()
-for (x, y), weight in weights.items():
-    for dx in range(-20, 21):
-        for dy in range(-20, 21):
-            cost = abs(dx) + abs(dy)
-            if cost <= 20:
-                new_x, new_y = x + dx, y + dy
-                if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]):
-                    if grid[new_x][new_y] != "#":
-                        if ((x, y), (new_x, new_y)) not in used_cheats:
-                            used_cheats.add(((x, y), (new_x, new_y)))
-                        new_time = bfs((new_x, new_y), weights[(x, y)] + cost)
-                        if new_time < initial_time:
-                            time_saved = initial_time - new_time
-                            if time_saved >= 100:
-                                part2 += 1
+valid_moves = [(dx, dy) for dx in range(-20, 21) for dy in range(-20, 21) if abs(dx) + abs(dy) <= 20]
+
+for pos, weight in weights.items():
+    for dx, dy in valid_moves:
+        new_pos = pos[0] + dx, pos[1] + dy
+        if is_in_bounds(*new_pos):
+            if grid[new_pos[0]][new_pos[1]] != "#":
+                if (pos, new_pos) not in used_cheats:
+                    used_cheats.add((pos, new_pos))
+
+                cost = abs(dx) + abs(dy)
+                new_time = bfs(new_pos, weights[pos] + cost)
+
+                time_saved = initial_time - new_time
+                if time_saved >= 100:
+                    part2 += 1
 
 print("Part 2:", part2)
